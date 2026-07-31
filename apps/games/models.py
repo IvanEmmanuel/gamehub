f3c5d75adef2f8662a29b8cc748fa3827a5cc291
Iván <lti_ivan_hernandez@hotmail.com>
2026-07-30 23:32:04 -0600

    Add user game library models

--- apps/games/models.py
+++ apps/games/models.py
@@ -2,3 +2,4 @@ from django.db import models
 from .models.genre import Genre
 from .models.game import Game
 from .models.review import Review
+from .models.userGameLibrary import UserGameLibrary
--- apps/games/models/__init__.py
+++ apps/games/models/__init__.py
@@ -1,3 +1,4 @@
 from .genre import Genre
 from .game import Game
 from .review import Review
+from .userGameLibrary import UserGameLibrary
